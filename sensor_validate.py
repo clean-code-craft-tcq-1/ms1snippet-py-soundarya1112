5629b18e822f05b2f716153936475481bfb75f9e
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-03-22 19:22:27 +0530

    Update sensor_validate.py

--- sensor_validate.py
+++ sensor_validate.py
@@ -9,11 +9,7 @@ def validate_soc_reading(values):
   for i in range(last_but_one_reading):
     if(not _give_me_a_good_name(values[i], values[i + 1], 0.05)):
       return False
-  return True
-
-def validate_current_reading(values):
-  last_but_one_reading = len(values) - 1
-  for i in range(last_but_one_reading):
-    if(not _give_me_a_good_name(values[i], values[i + 1], 0.1)):
+    elif(not _give_me_a_good_name(values[i], values[i + 1], 0.1)):
       return False
-  return True
+    else
+      return True
