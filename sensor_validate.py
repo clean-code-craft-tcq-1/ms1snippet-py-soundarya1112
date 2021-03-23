be29066258549eaf7bddc10e7fbe168042b62ce8
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-03-23 15:18:28 +0530

    Update sensor_validate.py

--- sensor_validate.py
+++ sensor_validate.py
@@ -4,12 +4,13 @@ def  Setting_up_thresholds(value, nextValue, maxDelta):
     return False
   return True
 
-def validate_soc_reading(values is not None):
+def validate_soc_reading(values):
   last_but_one_reading = len(values) - 1
   for i in range(last_but_one_reading):
     if(not Setting_up_thresholds(values[i], values[i + 1], 0.05)):
       return False
     elif(not Setting_up_thresholds(values[i], values[i + 1], 0.1)):
       return True
+    elif values is None
    
     
