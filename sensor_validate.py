71270752192b3c82770170d2d260830c7c270bfd
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-03-24 16:49:22 +0530

    Update sensor_validate.py

--- sensor_validate.py
+++ sensor_validate.py
@@ -11,10 +11,10 @@ def validate_none(values):
   return False
 
 def validate_soc_reading(values, sensor_param_name):
-  values = validate_none(values)
   last_but_one_reading = len(values) - 1
   for i in range(last_but_one_reading):
-    if(not Setting_up_thresholds(values[i], values[i + 1], sensor_limits[sensor_param_name])):
+    if validate_none(values)==False:
+      if(not Setting_up_thresholds(values[i], values[i + 1], sensor_limits[sensor_param_name])):
        return False
   return True
      
