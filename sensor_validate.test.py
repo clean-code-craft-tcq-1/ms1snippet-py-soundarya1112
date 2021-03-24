fac5879c6ca0a46d96a5d83fc172e861251bfa36
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-03-24 17:28:57 +0530

    Create sensor_validate.test.py

--- sensor_validate.test.py
+++ sensor_validate.test.py
@@ -3,16 +3,12 @@ import sensor_validate
 
 class SensorValidatorTest(unittest.TestCase):
   def test_reports_error_when_soc_jumps(self):
-    self.assertFalse(
-      sensor_validate.validate_soc_reading([0.0, 0.01, 0.5, 0.51],'soc')
-    )
+    self.assertFalse(sensor_validate.validate_soc_reading([0.0, 0.01, 0.5, 0.51],'soc'))
+    self.assertTrue(sensor_validate.validate_current_reading([],'soc'))
     
   def test_reports_error_when_current_jumps(self):
-    self.assertFalse(
-      sensor_validate. validate_soc_reading([0.03, 0.03, 0.03, 0.33],'curr')
-    )
-  def test_reports_error_when_current_jumps(self):
-    self.assertFalse(sensor_validate.validate_soc_reading([],'curr')== None)
+    self.assertFalse(sensor_validate. validate_soc_reading([0.03, 0.03, 0.03, 0.33],'curr'))
+    self.assertTrue(sensor_validate.validate_soc_reading([],'curr')== None)
 
 if __name__ == "__main__":
   unittest.main()
